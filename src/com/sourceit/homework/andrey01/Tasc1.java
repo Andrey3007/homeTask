72a37587b39a49e5a4b715b47555469848ba4be0
Andrey <kuznecov_95@mail.ua>
2015-02-10 20:25:32 +0200

    Original commit

---
+++ src/com/sourceit/homework/andrey01/Tasc1.java
@@ -0,0 +1,12 @@
+package com.sourceit.homework.andrey01;
+
+/**
+ * Created by Андрей on 10.02.2015.
+ */
+public class Tasc1 {
+    public static void main(String[] args){
+        int a;
+        char d;
+        System.out.print("");
+    }
+}
