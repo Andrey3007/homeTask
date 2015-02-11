68ab3b382ee6b5f8ea5f270e466fcdbaa4a7162f
Andrey <kuznecov_95@mail.ua>
2015-02-11 23:16:33 +0200

    Original commit

---
+++ src/com/sourceit/homework/andrey01/homeTasc03/array/Array.java
@@ -0,0 +1,18 @@
+package com.sourceit.homework.andrey01.homeTasc03.array;
+
+/**
+ * Created by Андрей on 11.02.2015.
+ */
+public class Array {
+    public static void main(String[] args) {
+        int a[][] = new int[8][5];
+        for (int k =0; k < 8 ; k ++) {
+            for (int i = 0; i < 5; i++) {
+               int d= ((int)(((Math.random())*(90))+(10)));
+                a[k][i]=d;
+                System.out.print(a[k][i]+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/com/sourceit/homework/andrey01/homeTasc03/array/Array2.java
@@ -0,0 +1,31 @@
+package com.sourceit.homework.andrey01.homeTasc03.array;
+
+/**
+ * Created by Андрей on 11.02.2015.
+ */
+public class Array2 {
+    public static void main(String[] args) {
+        int a[][] = new int[8][5];
+        int maxSum=0;
+        int index=0;
+        for (int k = 0; k < 8; k++) {
+            int sum=0;
+            for (int i = 0; i < 5; i++) {
+                int d = ((int) (((Math.random()) * (6))));
+                if (Math.random() < 0.5) {
+                    d = -d;
+                }
+                a[k][i] = d;
+                System.out.print(a[k][i] + "  ");
+                sum+= Math.abs(a[k][i]);
+            }
+            //System.out.print(sum+" "+k);  //Для ручной проверки правильности
+            System.out.println();
+            if(sum>maxSum){
+                maxSum=sum;
+                index=k;
+            }
+        }
+        System.out.println("Наибольшая сумма равна "+maxSum+"в строке №"+index);
+    }
+}
