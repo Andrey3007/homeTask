27accb11ece0e4377ec269048ad3d6db33e673d3
Andrey <kuznecov_95@mail.ua>
2015-02-14 09:37:28 +0200

    Original commit

--- src/com/sourceit/homework/andrey01/Tasc1.java
+++ src/com/sourceit/homework/andrey01/Tasc1.java
@@ -3,11 +3,15 @@ package com.sourceit.homework.andrey01;
 /**
  * Created by Андрей on 10.02.2015.
  * •	Создать класс, который содержит int и char (не проинициализированные) и распечатать их значения.
+ * Замечания:  - The idea of the task was to create fields (variables in a class) not
+ *                  the local variables (variables in a method).
  */
 public class Tasc1 {
+    static int a;           //Я тут исправил. Поставил переменные в статические поля класса.
+    static char d;
     public static void main(String[] args){
-        int a;
-        char d;
-        System.out.print("Их нельзя напечатать");
+        System.out.print(a);
+        System.out.print(d);
+        //System.out.print("Их нельзя напечатать");
     }
 }
