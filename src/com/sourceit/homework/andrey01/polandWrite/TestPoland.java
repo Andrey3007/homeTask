f248dc62a60f804c56408d3a2de14b73de193d60
Andrey <kuznecov_95@mail.ua>
2015-03-28 10:13:48 +0200

    Polan write

--- src/com/sourceit/homework/andrey01/polandWrite/TestPoland.java
+++ src/com/sourceit/homework/andrey01/polandWrite/TestPoland.java
@@ -9,7 +9,7 @@ public class TestPoland {
     public static void main(String[] args) {
         Poland poland = new Poland();
         Scanner scanner= new Scanner(System.in);
-        String expr= scanner.nextLine();
+        String expr= scanner.nextLine();         // (8+2*5)/(1+3*2-4)
         System.out.println(poland.fullPoland(expr));
 
     }
